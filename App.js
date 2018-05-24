217d87cb11a57852fc36958909dd1b5964ee9834
sleeplesseditor <josh@bryson.me.uk>
2018-05-24 15:42:56 +0100

    Initial stack navigation implementation

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingScreen from './screens/SettingScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = createBottomTabNavigator({
-      welcome: WelcomeScreen ,
-      auth: AuthScreen 
+      welcome: WelcomeScreen,
+      auth: AuthScreen,
+      main: {
+        screen: createBottomTabNavigator({
+          map: MapScreen,
+          deck: DeckScreen,
+          review: {
+            screen: createStackNavigator({
+              review: ReviewScreen,
+              settings: SettingScreen
+            })
+          }
+        })
+      } 
     });
 
     return (
