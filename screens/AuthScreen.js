30559d953a55d4fb1ea78d979024483963bf3f76
sleeplesseditor <josh@bryson.me.uk>
2018-05-29 14:56:02 +0100

    Auth reducer setup

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { AsyncStorage, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class AuthScreen extends Component {
     componentDidMount() {
         this.props.facebookLogin();
+        this.onAuthComplete(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.onAuthComplete(nextProps);
+    }
+
+    onAuthComplete(props) {
+        if (props.token) {
+            this.props.navigation.navigate('map');
+        }
     }
 
     render() {
         return (
-            <View>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-                <Text>AuthScreen</Text>
-            </View>
+            <View />
         );
     }
 }
 
-export default connect(null, actions)(AuthScreen);
+function mapStateToProps({ auth }) {
+    return { token: auth.token };
+}
+
+export default connect(mapStateToProps, actions)(AuthScreen);
