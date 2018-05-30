84c28dc958a79cddf5358d4d8ee5ac842eef5c6d
sleeplesseditor <josh@bryson.me.uk>
2018-05-30 11:42:53 +0100

    Job search button added to MapScreen

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A job finder app built using React Native. It uses React Navigation for navigating between screens, the Indeed API for search data, Expo for Facebook login.
 
-The app's use is limited to administrators and invited testers only, due to the restrictions of Facebook's developer platform.
+The app's use is limited to administrators and invited testers only, due to the restrictions of Facebook's developer platform. In addition, it is hardcoded to only search with specific criteria in a limited area, but has the potential for a search function to be added.
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { ActivityIndicator, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class MapScreen extends Component {
     state = {
@@ -22,6 +25,10 @@ class MapScreen extends Component {
         this.setState({ region });
     }
 
+    onButtonPress = () => {
+        this.props.fetchJobs(this.state.region);
+    }
+
     render() {
         if (!this.state.mapLoaded) {
             return (
@@ -37,9 +44,29 @@ class MapScreen extends Component {
                     style={{ flex: 1}}
                     onRegionChangeComplete={this.onRegionChangeComplete} 
                 />
+                <View style={styles.buttonContainer}>
+                    <Button 
+                        large
+                        title="Search This Area"
+                        backgroundColor="#009688"
+                        icon={{ name: 'search' }}
+                        onPress={this.onButtonPress}
+                    />
+                </View>
             </View>
         );
     }
 }
 
-export default MapScreen;
+
+const styles = {
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 20,
+        left: 0,
+        right: 0
+    }
+}
+
+
+export default connect(null, actions)(MapScreen);
