311fbd1039f6e74aabbf4b7edd4413accb8ef627
sleeplesseditor <josh@bryson.me.uk>
2018-06-01 11:47:08 +0100

    Push notification setup

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import Expo from 'expo';
+import Expo, { Notifications } from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import { Provider } from 'react-redux';
 
+import registerForNotifications from './services/push_notifications';
 import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -13,6 +14,21 @@ import DeckScreen from './screens/DeckScreen';
 import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const { data: { text }, origin } = notification;
+
+      if (origin === 'received' && text ) {
+        Alert.alert(
+          'New Push Notification',
+          text,
+          [{ text: 'OK' }]
+        );
+      }
+    });
+  }
+  
   render() {
     const MainNavigator = createBottomTabNavigator({
       welcome: WelcomeScreen,
--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # ReactNativeJobFinder
 
-A job finder app built using React Native. It uses React Navigation for navigating between screens, the Indeed API for search data, and Expo for Facebook login and push notifcations.
+A job finder app built using React Native. It uses React Navigation for navigating between screens, the Indeed API for search data, and Expo for Facebook login and push notifcations (along with Stephen Grider's rallycoding push notification API).
 
 The app's use is limited to administrators and invited testers only, due to the restrictions of Facebook's developer platform. In addition, it is hardcoded to only search with specific criteria in a limited area, but has the potential for a search function to be added.
+
+![screen shot 2018-06-01 at 10 57 27](https://user-images.githubusercontent.com/25869284/40835684-0dc38000-658c-11e8-80fa-d78e9227f996.png)
+
+Welcome Screen
+
 
 ![img_2812](https://user-images.githubusercontent.com/25869284/40834696-76cbb0f2-6589-11e8-8da6-ecd99d593ab7.PNG)
 
