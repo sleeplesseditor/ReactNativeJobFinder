d8e699c11131677f0dfc2ce67630bbe3b90ec91e
sleeplesseditor <josh@bryson.me.uk>
2018-06-01 09:27:42 +0100

    Data persistance setup

--- store/index.js
+++ store/index.js
@@ -1,13 +1,18 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { autoRehydrate, persistStore } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 import reducers from '../reducers';
 
 const store = createStore(
     reducers,
     {},
     compose(
-       applyMiddleware(thunk)
+       applyMiddleware(thunk),
+       autoRehydrate()
     )
 );
+
+persistStore(store, { storage: AsyncStorage, whitelist: ['likedJobs'] });
 
 export default store;
